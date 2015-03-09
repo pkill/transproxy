d18885ed1c8911a17bd48dfb1b10ffce779f2907
Alex Elman <pkill@linux.com>
2015-03-08 19:34:07 -0500

    Update squidconf for version 3+

--- tproxy_server_setup.sh
+++ tproxy_server_setup.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
-sudo yum install -y squid
+
+# Install dependencies
+sudo yum install -y squid git
+
+# Get transproxy
 cd $HOME
 git clone https://github.com/pkill/transproxy.git
+
+# Backup old squidconf and load config for transparent proxy
 sudo mv /etc/squid/squid.conf /etc/squid/squid.conf.$(date +%F)
 sudo cp transproxy/squid.conf /etc/squid/squid.conf
+
+# Make sure squid starts on startup
+sudo chkconfig squid on
+
+# Start squid if it's not already started
+sudo service squid restart
